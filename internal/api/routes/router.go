41fda1c44231d9df22afa9dfa884550b3d63f48a
Adrian Kurek <adikurek11@gmail.com>
2025-07-21 22:56:44 +0200

    APP CONFIG
    Luckily I found method how to do router

--- internal/api/routes/router.go
+++ internal/api/routes/router.go
@@ -1,22 +1,47 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/slodkiadrianek/octopus/internal/utils"
+)
 
 type (
 	Router     struct{}
 	Middleware func(http.Handler) http.Handler
 )
 
-func (r *Router) Get(route string, finalHandler http.Handler, fns ...Middleware) {
+func methodCheck(next http.Handler, method string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if method != r.Method {
+			utils.SendResponse(w, 404, map[string]string{"error": "Not found"})
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (r *Router) Get(route string, fns ...interface{}) {
+	middlewares := []Middleware{}
+	var finalHandler http.Handler
+	for _, el := range fns {
+		switch fn := el.(type) {
+		case func(http.Handler) http.Handler:
+			middlewares = append(middlewares, fn)
+		case http.Handler:
+			finalHandler = fn
+		}
+	}
+	handler := finalHandler
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		handler = middlewares[i](handler)
+	}
+	http.Handle(route, methodCheck(handler, "GET"))
+}
+
+func (r *Router) Post(route string, fns ...Middleware) {
 	handler := finalHandler
 	for i := len(fns) - 1; i >= 0; i-- {
 		handler = fns[i](handler)
 	}
-	http.HandleFunc(route, handler)
-}
-
-func (r *Router) Post(route string, fns ...Middleware) {
-	for _, fn := range fns {
-		http.HandleFunc(route, fn)
-	}
+	http.Handle(route, handler)
 }
