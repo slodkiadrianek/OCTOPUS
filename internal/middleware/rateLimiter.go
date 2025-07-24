9cd70a95a6b2f6e94b134f5795328c78f7ce784f
slodkiadrianek <adikurek11@gmail.com>
2025-07-24 23:01:58 +0200

    middleware
    rate limiter

--- internal/middleware/rateLimiter.go
+++ internal/middleware/rateLimiter.go
@@ -3,8 +3,43 @@ package middleware
 import "time"
 
 type RateLimiter struct {
-	Token      int
-	MaxTokens  int
-	refillRate time.Duration
-	lastRefill time.Duration
+	Tokens        int
+	MaxTokens     int
+	RefillRate    time.Duration
+	LastRefill    time.Time
+	BlockUntil    time.Time
+	BlockDuration time.Duration
+}
+
+func NewRateLimiter(maxTokens int, refillRate time.Duration, blockDuration time.Duration) *RateLimiter {
+	return &RateLimiter{
+		Tokens:        maxTokens,
+		MaxTokens:     maxTokens,
+		RefillRate:    refillRate,
+		LastRefill:    time.Now(),
+		BlockDuration: blockDuration,
+	}
+}
+
+func (ra *RateLimiter) Allow() bool {
+	now := time.Now()
+	if now.Before(ra.BlockUntil) {
+		return false
+	}
+	elapsed := now.Sub(ra.LastRefill)
+	newTokens := (elapsed / ra.RefillRate)
+	if newTokens > 0 {
+		ra.Tokens += int(newTokens)
+		if ra.Tokens > ra.MaxTokens {
+			ra.Tokens = ra.MaxTokens
+		}
+		ra.LastRefill = now
+	}
+
+	if ra.Tokens > 0 {
+		ra.Tokens--
+		return true
+	}
+
+	return false
 }
